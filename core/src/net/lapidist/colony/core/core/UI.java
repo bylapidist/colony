805251e2f5350f464ee89bc940b1b14b83f5a258
Brett Dorrans <hello@lapidist.net>
2018-11-05 21:49:17 +0000

    refactor(engine): Convert to local variables in UI

--- core/src/net/lapidist/colony/core/core/UI.java
+++ core/src/net/lapidist/colony/core/core/UI.java
@@ -7,23 +7,19 @@ import static net.lapidist.colony.core.Constants.*;
 
 public class UI extends SceneModule {
 
-    private Table table;
-    private Skin skin;
-
     public UI() {
-        skin = resourceLoader.getSkin("default");
-
-        Label label = new Label("Turn", skin);
-        TextField textField = new TextField("", skin);
+        Skin skin = resourceLoader.getSkin("default");
+        Label turnLabel = new Label("Turn", skin);
+        Label turnLabelCount = new Label("1", skin);
         TextButton nextTurnButton = new TextButton("Next", skin);
 
-        table = new Table();
+        Table table = new Table();
         table.setFillParent(true);
         table.right().bottom();
 //        table.setDebug(true);
 
-        table.add(label);
-        table.add(textField).width(200);
+        table.add(turnLabel);
+        table.add(turnLabelCount).width(50);
         table.add(nextTurnButton).width(100);
         table.toFront();
 
