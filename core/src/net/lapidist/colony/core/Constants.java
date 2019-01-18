3bca179addbb4c026f04da90af6d1124ce7164a3
Brett Dorrans <brett@usemarkup.com>
2019-01-18 06:46:26 +0000

    fix(engine): Dependency ordering

--- core/src/net/lapidist/colony/core/Constants.java
+++ core/src/net/lapidist/colony/core/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public static final String NAME = "Colony";
     public static final String VERSION = "1.0.0";
     public static final boolean DEBUG = true;
-    public static final int WIDTH = 1920;
-    public static final int HEIGHT = 1280;
+    public static final int WIDTH = 1080;
+    public static final int HEIGHT = 720;
     public static final int PPM = 64; // Pixels per meter
 }
