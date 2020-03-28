f9b9ef7e1d44666178ac1de8c05bb9891e525fc9
Brett Dorrans <hello@lapidist.net>
2020-03-28 21:33:22 +0000

    feat(console): add annotations package

---
+++ core/src/net/lapidist/colony/core/annotation/ConsoleDoc.java
@@ -0,0 +1,15 @@
+package net.lapidist.colony.core.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ConsoleDoc {
+
+    String description() default "";
+
+    String[] parameterDescriptions() default {};
+}
