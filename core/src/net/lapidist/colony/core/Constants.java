c772e77405aa4c5fadd7792a0536d2ea3c18b877
Brett Dorrans <hello@lapidist.net>
2021-07-01 18:33:57 +0100

    feat: remove server project (#102)

--- core/src/net/lapidist/colony/core/Constants.java
+++ core/src/net/lapidist/colony/core/Constants.java
@@ -11,7 +11,4 @@ public final class Constants {
     public static final int WIDTH = 1080;
     public static final int HEIGHT = 720;
     public static final int TARGET_FPS = 60;
-    public static final int DEFAULT_PORT = 9966;
-    public static final String DEFAULT_HOSTNAME = "127.0.0.1";
-    public static final int MAX_PACKET_SIZE = 10_000;
 }
