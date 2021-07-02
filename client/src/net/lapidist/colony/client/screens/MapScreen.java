67bf13a09059e73dab29d9d3d3df55158351d8b4
Brett Dorrans <hello@lapidist.net>
2021-07-02 20:46:19 +0100

    refactor: tidy up event handling (#104)
    * refactor: tidy up event handling
    
    * chore: checkstyle
    
    * chore: revert .idea/runConfigurations/UnitTest.xml

--- client/src/net/lapidist/colony/client/screens/MapScreen.java
+++ client/src/net/lapidist/colony/client/screens/MapScreen.java
@@ -8,7 +8,9 @@ import com.artemis.managers.TagManager;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import net.lapidist.colony.client.systems.ClearScreenSystem;
+import net.lapidist.colony.core.events.EventType;
 import net.lapidist.colony.core.events.Events;
+import net.lapidist.colony.core.events.payloads.ResizePayload;
 
 public class MapScreen implements Screen {
 
@@ -36,27 +38,27 @@ public class MapScreen implements Screen {
 
     @Override
     public final void resize(final int width, final int height) {
-        Events.dispatch(0, Events.EventType.RESIZE);
+        Events.dispatch(EventType.RESIZE, new ResizePayload(width, height));
     }
 
     @Override
     public final void pause() {
-        Events.dispatch(0,  Events.EventType.PAUSE);
+        Events.dispatch(EventType.PAUSE);
     }
 
     @Override
     public final void resume() {
-        Events.dispatch(0,  Events.EventType.RESUME);
+        Events.dispatch(EventType.RESUME);
     }
 
     @Override
     public final void hide() {
-        Events.dispatch(0, Events.EventType.HIDE);
+        Events.dispatch(EventType.HIDE);
     }
 
     @Override
     public final void show() {
-        Events.dispatch(0, Events.EventType.SHOW);
+        Events.dispatch(EventType.SHOW);
     }
 
     @Override
---
+++ core/src/net/lapidist/colony/core/events/EventType.java
@@ -0,0 +1,32 @@
+package net.lapidist.colony.core.events;
+
+public enum EventType {
+    GAME_INIT(0, "Game Init"),
+    PAUSE(1, "Pause"),
+    RESUME(2, "Resume"),
+    RESIZE(3, "Resize"),
+    HIDE(4, "Hide"),
+    SHOW(5, "Show");
+
+    private final int ordinal;
+
+    private final String name;
+
+    EventType(final int ordinalToSet, final String nameToSet) {
+        this.ordinal = ordinalToSet;
+        this.name = nameToSet;
+    }
+
+    public int getOrdinal() {
+        return this.ordinal;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s:%s", getOrdinal(), getName());
+    }
+}
