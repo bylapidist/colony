6c20bf9a7419de43a408009f18a2393c80e23da4
Brett Dorrans <hello@lapidist.net>
2023-06-03 16:52:37 +0100

    feat: BuildingComponent

---
+++ components/src/net/lapidist/colony/components/entities/BuildingComponent.java
@@ -0,0 +1,68 @@
+package net.lapidist.colony.components.entities;
+
+import com.badlogic.ashley.core.Component;
+
+public class BuildingComponent implements Component {
+
+    public enum BuildingType {
+        HOUSE("House"),
+        MARKET("Market"),
+        FACTORY("Factory");
+
+        private final String type;
+
+        BuildingType(final String typeToSet) {
+            this.type = typeToSet;
+        }
+
+        @Override
+        public String toString() {
+            return type;
+        }
+    }
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private BuildingType buildingType;
+
+    public final int getX() {
+        return x;
+    }
+
+    public final void setX(final int xToSet) {
+        this.x = xToSet;
+    }
+
+    public final int getY() {
+        return y;
+    }
+
+    public final void setY(final int yToSet) {
+        this.y = yToSet;
+    }
+
+    public final int getWidth() {
+        return width;
+    }
+
+    public final void setWidth(final int widthToSet) {
+        this.width = widthToSet;
+    }
+
+    public final int getHeight() {
+        return height;
+    }
+
+    public final void setHeight(final int heightToSet) {
+        this.height = heightToSet;
+    }
+
+    public final BuildingType getBuildingType() {
+        return buildingType;
+    }
+
+    public final void setBuildingType(final BuildingType buildingTypeToSet) {
+        this.buildingType = buildingTypeToSet;
+    }
+}
--- components/src/net/lapidist/colony/components/maps/MapComponent.java
+++ components/src/net/lapidist/colony/components/maps/MapComponent.java
@@ -8,11 +8,34 @@ public class MapComponent implements Component {
 
     private Array<Entity> tiles;
 
+    private Array<Entity> entities;
+
     public final Array<Entity> getTiles() {
         return tiles;
     }
 
+    public final Array<Entity> getEntities() {
+        return entities;
+    }
+
     public final void setTiles(final Array<Entity> tilesToSet) {
         this.tiles = tilesToSet;
+    }
+
+    public final void setEntities(final Array<Entity> entitiesToSet) {
+        this.entities = entitiesToSet;
+    }
+
+    public final void addEntity(final Entity entityToAdd) {
+        this.entities.add(entityToAdd);
+    }
+
+    public final void removeEntity(final Entity entityToRemove) {
+        this.entities.removeValue(entityToRemove, true);
+    }
+
+    public final void reset() {
+        this.tiles.clear();
+        this.entities.clear();
     }
 }
