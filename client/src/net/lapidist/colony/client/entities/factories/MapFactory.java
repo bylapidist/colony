7220cd9490d595bfea0091a39771a5fb14e5c543
Brett Dorrans <hello@lapidist.net>
2025-06-08 00:32:30 +0100

    feat(client): extract map creation into MapFactory

---
+++ client/src/net/lapidist/colony/client/entities/factories/MapFactory.java
@@ -0,0 +1,59 @@
+package net.lapidist.colony.client.entities.factories;
+
+import com.artemis.Entity;
+import com.artemis.World;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import net.lapidist.colony.components.entities.BuildingComponent;
+import net.lapidist.colony.components.maps.MapComponent;
+import net.lapidist.colony.components.maps.TileComponent;
+import net.lapidist.colony.components.state.BuildingData;
+import net.lapidist.colony.components.state.MapState;
+import net.lapidist.colony.components.state.TileData;
+
+/**
+ * Utility factory for creating map entities from {@link MapState} data.
+ */
+public final class MapFactory {
+
+    private MapFactory() { }
+
+    /**
+     * Create a new map entity populated with tiles and buildings from the given state.
+     *
+     * @param world the Artemis world to create entities in
+     * @param state the map state to convert to entities
+     * @return the created map entity containing a {@link MapComponent}
+     */
+    public static Entity create(final World world, final MapState state) {
+        Entity map = world.createEntity();
+        MapComponent mapComponent = new MapComponent();
+        Array<Entity> tiles = new Array<>();
+        Array<Entity> entities = new Array<>();
+
+        for (TileData td : state.getTiles()) {
+            tiles.add(TileFactory.create(
+                    world,
+                    TileComponent.TileType.valueOf(td.getTileType()),
+                    td.getTextureRef(),
+                    new Vector2(td.getX(), td.getY()),
+                    td.isPassable(),
+                    td.isSelected()
+            ));
+        }
+
+        for (BuildingData bd : state.getBuildings()) {
+            entities.add(BuildingFactory.create(
+                    world,
+                    BuildingComponent.BuildingType.valueOf(bd.getBuildingType()),
+                    bd.getTextureRef(),
+                    new Vector2(bd.getX(), bd.getY())
+            ));
+        }
+
+        mapComponent.setTiles(tiles);
+        mapComponent.setEntities(entities);
+        map.edit().add(mapComponent);
+        return map;
+    }
+}
--- client/src/net/lapidist/colony/client/systems/MapGenerationSystem.java
+++ client/src/net/lapidist/colony/client/systems/MapGenerationSystem.java
@@ -1,14 +1,7 @@
 package net.lapidist.colony.client.systems;
 
 import com.artemis.BaseSystem;
-import com.artemis.Entity;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.utils.Array;
-import net.lapidist.colony.client.entities.factories.TileFactory;
-import net.lapidist.colony.client.entities.factories.BuildingFactory;
-import net.lapidist.colony.components.entities.BuildingComponent;
-import net.lapidist.colony.components.maps.MapComponent;
-import net.lapidist.colony.components.maps.TileComponent;
+import net.lapidist.colony.client.entities.factories.MapFactory;
 import net.lapidist.colony.map.MapGenerator;
 
 public final class MapGenerationSystem extends BaseSystem {
@@ -24,36 +17,8 @@ public final class MapGenerationSystem extends BaseSystem {
 
     @Override
     public void initialize() {
-        Array<Entity> tilesToSet = new Array<>();
-        Array<Entity> entitiesToSet = new Array<>();
-        Entity map = world.createEntity();
-        MapComponent mapComponent = new MapComponent();
-
         var state = MapGenerator.generate(mapWidth, mapHeight);
-
-        for (var td : state.getTiles()) {
-            tilesToSet.add(TileFactory.create(
-                    world,
-                    TileComponent.TileType.valueOf(td.getTileType()),
-                    td.getTextureRef(),
-                    new Vector2(td.getX(), td.getY()),
-                    td.isPassable(),
-                    td.isSelected()
-            ));
-        }
-
-        for (var bd : state.getBuildings()) {
-            entitiesToSet.add(BuildingFactory.create(
-                    world,
-                    BuildingComponent.BuildingType.valueOf(bd.getBuildingType()),
-                    bd.getTextureRef(),
-                    new Vector2(bd.getX(), bd.getY())
-            ));
-        }
-
-        mapComponent.setTiles(tilesToSet);
-        mapComponent.setEntities(entitiesToSet);
-        map.edit().add(mapComponent);
+        MapFactory.create(world, state);
     }
 
     @Override
