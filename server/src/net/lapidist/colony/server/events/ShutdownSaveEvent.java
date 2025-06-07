f75ea7968aa9c24b22a42a2fe76fb36b3164ecd8
Brett Dorrans <hello@lapidist.net>
2025-06-08 00:05:09 +0100

    refactor(server): deduplicate save events (#291)

--- server/src/net/lapidist/colony/server/events/ShutdownSaveEvent.java
+++ server/src/net/lapidist/colony/server/events/ShutdownSaveEvent.java
@@ -1,36 +1,13 @@
 package net.lapidist.colony.server.events;
 
-import net.mostlyoriginal.api.event.common.Event;
-
 import java.nio.file.Path;
 
 /**
  * Event fired when the server saves the game on shutdown.
  */
-public final class ShutdownSaveEvent implements Event {
-    private final Path location;
-    private final long size;
+public final class ShutdownSaveEvent extends SaveEvent {
 
     public ShutdownSaveEvent(final Path locationToSet, final long sizeToSet) {
-        this.location = locationToSet;
-        this.size = sizeToSet;
-    }
-
-    public Path getLocation() {
-        return location;
-    }
-
-    public long getSize() {
-        return size;
-    }
-
-    @Override
-    public String toString() {
-        return String.format(
-                "%s(location=%s, size=%d)",
-                getClass().getSimpleName(),
-                location,
-                size
-        );
+        super(locationToSet, sizeToSet);
     }
 }
