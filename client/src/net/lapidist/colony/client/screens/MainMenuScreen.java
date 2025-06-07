702d1909819e9102d710925966ae6ab0685ec349
Brett Dorrans <hello@lapidist.net>
2025-06-07 23:00:16 +0100

    feat(client): introduce base screen for shared UI logic (#278)

--- client/src/net/lapidist/colony/client/screens/MainMenuScreen.java
+++ client/src/net/lapidist/colony/client/screens/MainMenuScreen.java
@@ -1,15 +1,9 @@
 package net.lapidist.colony.client.screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import net.lapidist.colony.client.Colony;
 import net.lapidist.colony.io.Paths;
 
@@ -17,24 +11,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public final class MainMenuScreen extends ScreenAdapter {
-
-    private final Stage stage;
+public final class MainMenuScreen extends BaseScreen {
     private final Colony colony;
 
     public MainMenuScreen(final Colony game) {
         this.colony = game;
-        stage = new Stage(new ScreenViewport());
-        Skin skin = new Skin(Gdx.files.internal("skin/default.json"));
 
-        Table table = new Table();
-        table.setFillParent(true);
-        stage.addActor(table);
-
-        TextButton continueButton = new TextButton("Continue", skin);
-        TextButton newGameButton = new TextButton("New Game", skin);
-        TextButton loadGameButton = new TextButton("Load Game", skin);
-        TextButton exitButton = new TextButton("Exit", skin);
+        TextButton continueButton = new TextButton("Continue", getSkin());
+        TextButton newGameButton = new TextButton("New Game", getSkin());
+        TextButton loadGameButton = new TextButton("Load Game", getSkin());
+        TextButton exitButton = new TextButton("Exit", getSkin());
 
         String lastSave = null;
         boolean canContinue = false;
@@ -50,12 +36,10 @@ public final class MainMenuScreen extends ScreenAdapter {
 
         continueButton.setDisabled(!canContinue);
 
-        table.add(continueButton).row();
-        table.add(newGameButton).row();
-        table.add(loadGameButton).row();
-        table.add(exitButton).row();
-
-        Gdx.input.setInputProcessor(stage);
+        getRoot().add(continueButton).row();
+        getRoot().add(newGameButton).row();
+        getRoot().add(loadGameButton).row();
+        getRoot().add(exitButton).row();
 
         final String last = lastSave;
         continueButton.addListener(new ChangeListener() {
@@ -89,20 +73,4 @@ public final class MainMenuScreen extends ScreenAdapter {
         });
     }
 
-    @Override
-    public void render(final float delta) {
-        ScreenUtils.clear(0f, 0f, 0f, 1f);
-        stage.act(delta);
-        stage.draw();
-    }
-
-    @Override
-    public void resize(final int width, final int height) {
-        stage.getViewport().update(width, height, true);
-    }
-
-    @Override
-    public void dispose() {
-        stage.dispose();
-    }
 }
