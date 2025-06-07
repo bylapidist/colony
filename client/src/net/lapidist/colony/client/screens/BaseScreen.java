741d5de5b4ea5da85300c1b35598556f7a9c84b4
Brett Dorrans <hello@lapidist.net>
2025-06-07 22:58:33 +0100

    feat(client): introduce base screen for shared UI logic

---
+++ client/src/net/lapidist/colony/client/screens/BaseScreen.java
@@ -0,0 +1,57 @@
+package net.lapidist.colony.client.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+/**
+ * Base class for simple UI screens.
+ */
+public abstract class BaseScreen extends ScreenAdapter {
+
+    private final Stage stage;
+    private final Skin skin;
+    private final Table root;
+
+    protected BaseScreen() {
+        stage = new Stage(new ScreenViewport());
+        skin = new Skin(Gdx.files.internal("skin/default.json"));
+        root = new Table();
+        root.setFillParent(true);
+        stage.addActor(root);
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    protected final Stage getStage() {
+        return stage;
+    }
+
+    protected final Skin getSkin() {
+        return skin;
+    }
+
+    protected final Table getRoot() {
+        return root;
+    }
+
+    @Override
+    public final void render(final float delta) {
+        ScreenUtils.clear(0f, 0f, 0f, 1f);
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public final void resize(final int width, final int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public final void dispose() {
+        stage.dispose();
+    }
+}
--- client/src/net/lapidist/colony/client/screens/LoadGameScreen.java
+++ client/src/net/lapidist/colony/client/screens/LoadGameScreen.java
@@ -1,15 +1,8 @@
 package net.lapidist.colony.client.screens;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import net.lapidist.colony.client.Colony;
 import net.lapidist.colony.server.io.Paths;
 
@@ -17,24 +10,16 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public final class LoadGameScreen extends ScreenAdapter {
-
-    private final Stage stage;
+public final class LoadGameScreen extends BaseScreen {
     private final Colony colony;
 
     public LoadGameScreen(final Colony game) {
         this.colony = game;
-        stage = new Stage(new ScreenViewport());
-        Skin skin = new Skin(Gdx.files.internal("skin/default.json"));
-
-        Table table = new Table();
-        table.setFillParent(true);
-        stage.addActor(table);
 
         List<String> saves = listSaves();
         for (String save : saves) {
-            TextButton button = new TextButton(save, skin);
-            table.add(button).row();
+            TextButton button = new TextButton(save, getSkin());
+            getRoot().add(button).row();
             button.addListener(new ChangeListener() {
                 @Override
                 public void changed(final ChangeEvent event, final Actor actor) {
@@ -43,16 +28,14 @@ public final class LoadGameScreen extends ScreenAdapter {
             });
         }
 
-        TextButton backButton = new TextButton("Back", skin);
-        table.add(backButton).row();
+        TextButton backButton = new TextButton("Back", getSkin());
+        getRoot().add(backButton).row();
         backButton.addListener(new ChangeListener() {
             @Override
             public void changed(final ChangeEvent event, final Actor actor) {
                 colony.setScreen(new MainMenuScreen(colony));
             }
         });
-
-        Gdx.input.setInputProcessor(stage);
     }
 
     private List<String> listSaves() {
@@ -63,20 +46,4 @@ public final class LoadGameScreen extends ScreenAdapter {
         }
     }
 
-    @Override
-    public void render(final float delta) {
-        ScreenUtils.clear(0f, 0f, 0f, 1f);
-        stage.act(delta);
-        stage.draw();
-    }
-
-    @Override
-    public void resize(final int width, final int height) {
-        stage.getViewport().update(width, height, true);
-    }
-
-    @Override
-    public void dispose() {
-        stage.dispose();
-    }
 }
