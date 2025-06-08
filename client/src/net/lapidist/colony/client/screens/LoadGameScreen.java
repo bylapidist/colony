9295d73fd5eef47a4defc28f5ccddd70d823b5b5
Brett Dorrans <hello@lapidist.net>
2025-06-08 01:13:14 +0100

    feat(client): add save deletion with confirmation

--- client/src/net/lapidist/colony/client/screens/LoadGameScreen.java
+++ client/src/net/lapidist/colony/client/screens/LoadGameScreen.java
@@ -1,6 +1,8 @@
 package net.lapidist.colony.client.screens;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import net.lapidist.colony.client.Colony;
@@ -11,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class LoadGameScreen extends BaseScreen {
+    private static final float PADDING = 10f;
     private final Colony colony;
 
     public LoadGameScreen(final Colony game) {
@@ -18,12 +21,40 @@ public final class LoadGameScreen extends BaseScreen {
 
         List<String> saves = listSaves();
         for (String save : saves) {
-            TextButton button = new TextButton(save, getSkin());
-            getRoot().add(button).row();
-            button.addListener(new ChangeListener() {
+            TextButton loadButton = new TextButton(save, getSkin());
+            TextButton deleteButton = new TextButton("Delete", getSkin());
+            Table row = new Table();
+            row.add(loadButton).padRight(PADDING);
+            row.add(deleteButton);
+            getRoot().add(row).row();
+
+            loadButton.addListener(new ChangeListener() {
                 @Override
                 public void changed(final ChangeEvent event, final Actor actor) {
                     colony.startGame(save);
+                }
+            });
+
+            deleteButton.addListener(new ChangeListener() {
+                @Override
+                public void changed(final ChangeEvent event, final Actor actor) {
+                    Dialog confirm = new Dialog("Delete Save", getSkin(), "dialog") {
+                        @Override
+                        protected void result(final Object obj) {
+                            if (Boolean.TRUE.equals(obj)) {
+                                try {
+                                    Paths.deleteAutosave(save);
+                                } catch (IOException e) {
+                                    // ignore
+                                }
+                                colony.setScreen(new LoadGameScreen(colony));
+                            }
+                        }
+                    };
+                    confirm.text("Are you sure?");
+                    confirm.button("Yes", true);
+                    confirm.button("No", false);
+                    confirm.show(getStage());
                 }
             });
         }
