0c5874881008351b051eaf2771ed42dc7c4d6c3d
Brett Dorrans <hello@lapidist.net>
2025-06-08 01:10:52 +0100

    test: add scenario helpers for input

---
+++ tests/src/net/lapidist/colony/tests/scenario/GameSimulationCameraTest.java
@@ -0,0 +1,47 @@
+package net.lapidist.colony.tests.scenario;
+
+import net.lapidist.colony.client.core.Constants;
+import net.lapidist.colony.components.state.MapState;
+import net.lapidist.colony.components.state.TileData;
+import net.lapidist.colony.tests.GdxTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example scenario test verifying camera movement when panning.
+ */
+@RunWith(GdxTestRunner.class)
+public class GameSimulationCameraTest {
+
+    @Test
+    public void panMovesCamera() {
+        MapState state = new MapState();
+        TileData tile = new TileData();
+        tile.setX(0);
+        tile.setY(0);
+        tile.setTileType("GRASS");
+        tile.setTextureRef("grass0");
+        tile.setPassable(true);
+        state.getTiles().add(tile);
+
+        GameSimulation sim = new GameSimulation(state);
+        float startX = sim.getCamera().getCamera().position.x;
+        float startY = sim.getCamera().getCamera().position.y;
+
+        final float deltaX = -20f;
+        final float deltaY = 15f;
+        sim.getInput().pan(0, 0, deltaX, deltaY);
+        sim.step();
+
+        final float epsilon = 0.01f;
+        float expectedX = max(0, min(Constants.MAP_WIDTH * Constants.TILE_SIZE, startX - deltaX));
+        float expectedY = max(0, min(Constants.MAP_HEIGHT * Constants.TILE_SIZE, startY + deltaY));
+
+        assertEquals(expectedX, sim.getCamera().getCamera().position.x, epsilon);
+        assertEquals(expectedY, sim.getCamera().getCamera().position.y, epsilon);
+    }
+}
