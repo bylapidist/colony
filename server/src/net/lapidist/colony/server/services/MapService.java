e9e61098ea31ebe5337dd97851cf77e8d213b920
Brett Dorrans <hello@lapidist.net>
2025-06-08 15:08:19 +0100

    refactor(server): extract services from GameServer

---
+++ server/src/net/lapidist/colony/server/services/MapService.java
@@ -0,0 +1,55 @@
+package net.lapidist.colony.server.services;
+
+import net.lapidist.colony.components.GameConstants;
+import net.lapidist.colony.components.state.MapState;
+import net.lapidist.colony.io.Paths;
+import net.lapidist.colony.map.MapGenerator;
+import net.lapidist.colony.server.io.GameStateIO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * Loads existing maps or generates new ones when no save exists.
+ */
+public final class MapService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MapService.class);
+
+    private final MapGenerator mapGenerator;
+    private final String saveName;
+
+    public MapService(final MapGenerator generator, final String name) {
+        this.mapGenerator = generator;
+        this.saveName = name;
+    }
+
+    public MapState load() throws IOException {
+        Path saveFile = Paths.getAutosave(saveName);
+        MapState state;
+        if (Files.exists(saveFile)) {
+            state = GameStateIO.load(saveFile);
+            LOGGER.info("Loaded save file: {}", saveFile);
+        } else {
+            state = generateMap();
+            GameStateIO.save(state, saveFile);
+            LOGGER.info("Generated new map and saved to: {}", saveFile);
+        }
+        state = state.toBuilder()
+                .saveName(saveName)
+                .autosaveName(saveName + Paths.AUTOSAVE_SUFFIX)
+                .build();
+        Files.writeString(Paths.getLastAutosaveMarker(), saveName);
+        return state;
+    }
+
+    private MapState generateMap() {
+        return mapGenerator.generate(
+                GameConstants.MAP_WIDTH,
+                GameConstants.MAP_HEIGHT
+        );
+    }
+}
---
+++ server/src/net/lapidist/colony/server/services/NetworkService.java
@@ -0,0 +1,63 @@
+package net.lapidist.colony.server.services;
+
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+import com.esotericsoftware.kryonet.Server;
+import net.lapidist.colony.components.state.MapState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.function.Consumer;
+
+/**
+ * Manages network connections for the game server.
+ */
+public final class NetworkService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NetworkService.class);
+
+    private final Server server;
+    private final int tcpPort;
+    private final int udpPort;
+
+    public NetworkService(final Server serverToUse, final int tcp, final int udp) {
+        this.server = serverToUse;
+        this.tcpPort = tcp;
+        this.udpPort = udp;
+    }
+
+    /**
+     * Starts the server and sets up basic listeners.
+     *
+     * @param mapState   the current map state to send on connect
+     * @param dispatcher dispatcher for incoming messages
+     */
+    public void start(final MapState mapState, final Consumer<Object> dispatcher) throws IOException {
+        server.start();
+        LOGGER.info("Server started on TCP {} UDP {}", tcpPort, udpPort);
+        server.bind(tcpPort, udpPort);
+
+        server.addListener(new Listener() {
+            @Override
+            public void connected(final Connection connection) {
+                LOGGER.info("Connection established: {}", connection.getID());
+                connection.sendTCP(mapState);
+                LOGGER.info("Sent map state to connection {}", connection.getID());
+            }
+
+            @Override
+            public void received(final Connection connection, final Object object) {
+                dispatcher.accept(object);
+            }
+        });
+    }
+
+    public void stop() {
+        server.stop();
+    }
+
+    public void send(final Object message) {
+        server.sendToAllTCP(message);
+    }
+}
