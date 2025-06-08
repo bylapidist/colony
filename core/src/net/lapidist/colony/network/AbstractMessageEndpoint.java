347b66d3ce2c5a788718dd0ac8cb3e8d773bc684
Brett Dorrans <hello@lapidist.net>
2025-06-08 10:37:48 +0100

    refactor(network): share dispatcher logic

---
+++ core/src/net/lapidist/colony/network/AbstractMessageEndpoint.java
@@ -0,0 +1,26 @@
+package net.lapidist.colony.network;
+
+import java.util.function.Consumer;
+
+/**
+ * Base implementation of {@link MessageEndpoint} providing common dispatch
+ * logic for client and server endpoints.
+ */
+public abstract class AbstractMessageEndpoint implements MessageEndpoint {
+
+    private final MessageDispatcher dispatcher = new MessageDispatcher();
+
+    /**
+     * Dispatches a received message to registered handlers.
+     *
+     * @param message the message to dispatch
+     */
+    protected void dispatch(final Object message) {
+        dispatcher.dispatch(message);
+    }
+
+    @Override
+    public final <T> void onMessage(final Class<T> type, final Consumer<T> handler) {
+        dispatcher.register(type, handler);
+    }
+}
