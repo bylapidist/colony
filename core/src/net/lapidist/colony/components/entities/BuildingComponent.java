8b556cafa1e272b8a5e372dc67ce69b600c1c41d
Brett Dorrans <hello@lapidist.net>
2025-06-10 19:27:47 +0100

    refactor: extract shared bounded component

--- core/src/net/lapidist/colony/components/entities/BuildingComponent.java
+++ core/src/net/lapidist/colony/components/entities/BuildingComponent.java
@@ -1,9 +1,8 @@
 package net.lapidist.colony.components.entities;
 
-import com.artemis.Component;
-import net.lapidist.colony.components.BoundedComponent;
+import net.lapidist.colony.components.AbstractBoundedComponent;
 
-public class BuildingComponent extends Component implements BoundedComponent {
+public class BuildingComponent extends AbstractBoundedComponent {
 
     public enum BuildingType {
         HOUSE("House"),
@@ -21,43 +20,7 @@ public class BuildingComponent extends Component implements BoundedComponent {
             return type;
         }
     }
-    private int x;
-    private int y;
-    private int width;
-    private int height;
     private BuildingType buildingType;
-
-    public final int getX() {
-        return x;
-    }
-
-    public final void setX(final int xToSet) {
-        this.x = xToSet;
-    }
-
-    public final int getY() {
-        return y;
-    }
-
-    public final void setY(final int yToSet) {
-        this.y = yToSet;
-    }
-
-    public final int getWidth() {
-        return width;
-    }
-
-    public final void setWidth(final int widthToSet) {
-        this.width = widthToSet;
-    }
-
-    public final int getHeight() {
-        return height;
-    }
-
-    public final void setHeight(final int heightToSet) {
-        this.height = heightToSet;
-    }
 
     public final BuildingType getBuildingType() {
         return buildingType;
