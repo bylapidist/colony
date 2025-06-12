44d4a84d8a2f89755766c06d5e79cbcd598b0941
Brett Dorrans <hello@lapidist.net>
2025-06-12 20:31:00 +0100

    docs(perf): update benchmark results

---
+++ tests/src/test/java/net/lapidist/colony/tests/client/renderers/RendererEnumMapTest.java
@@ -0,0 +1,62 @@
+package net.lapidist.colony.tests.client.renderers;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import net.lapidist.colony.client.renderers.BuildingRenderer;
+import net.lapidist.colony.client.renderers.DefaultAssetResolver;
+import net.lapidist.colony.client.renderers.TileRenderer;
+import net.lapidist.colony.client.core.io.ResourceLoader;
+import net.lapidist.colony.client.systems.CameraProvider;
+import net.lapidist.colony.components.entities.BuildingComponent;
+import net.lapidist.colony.components.maps.TileComponent;
+import net.lapidist.colony.tests.GdxTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.lang.reflect.Field;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(GdxTestRunner.class)
+public class RendererEnumMapTest {
+
+    @Test
+    public void tileRendererCreatesEntriesForAllEnums() throws Exception {
+        SpriteBatch batch = mock(SpriteBatch.class);
+        ResourceLoader loader = mock(ResourceLoader.class);
+        when(loader.findRegion(anyString())).thenReturn(new TextureRegion());
+        TileRenderer renderer = new TileRenderer(batch, loader, mock(CameraProvider.class), new DefaultAssetResolver());
+
+        Field f = TileRenderer.class.getDeclaredField("tileRegions");
+        f.setAccessible(true);
+        java.util.Map<?, ?> map = (java.util.Map<?, ?>) f.get(renderer);
+
+        assertEquals(TileComponent.TileType.values().length, map.size());
+        for (TileComponent.TileType type : TileComponent.TileType.values()) {
+            assertNotNull(map.get(type));
+        }
+    }
+
+    @Test
+    public void buildingRendererCreatesEntriesForAllEnums() throws Exception {
+        SpriteBatch batch = mock(SpriteBatch.class);
+        ResourceLoader loader = mock(ResourceLoader.class);
+        when(loader.findRegion(anyString())).thenReturn(new TextureRegion());
+        BuildingRenderer renderer = new BuildingRenderer(
+                batch,
+                loader,
+                mock(CameraProvider.class),
+                new DefaultAssetResolver()
+        );
+
+        Field f = BuildingRenderer.class.getDeclaredField("buildingRegions");
+        f.setAccessible(true);
+        java.util.Map<?, ?> map = (java.util.Map<?, ?>) f.get(renderer);
+
+        assertEquals(BuildingComponent.BuildingType.values().length, map.size());
+        for (BuildingComponent.BuildingType type : BuildingComponent.BuildingType.values()) {
+            assertNotNull(map.get(type));
+        }
+    }
+}
