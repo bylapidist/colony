10bc62627b252b0ee926268e6a07da7387a6a89d
Brett Dorrans <hello@lapidist.net>
2025-06-14 09:10:03 +0100

    feat: add scrolling chat log with history limit (#620)

--- tests/src/test/java/net/lapidist/colony/tests/ui/ChatBoxTest.java
+++ tests/src/test/java/net/lapidist/colony/tests/ui/ChatBoxTest.java
@@ -1,7 +1,10 @@
 package net.lapidist.colony.tests.ui;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import net.lapidist.colony.chat.ChatMessage;
 import net.lapidist.colony.client.network.GameClient;
 import net.lapidist.colony.client.ui.ChatBox;
@@ -33,5 +36,40 @@ public class ChatBoxTest {
         com.badlogic.gdx.scenes.scene2d.ui.TextArea log =
                 (com.badlogic.gdx.scenes.scene2d.ui.TextArea) logField.get(box);
         assertTrue(log.getText().contains("1: hi"));
+    }
+
+    @Test
+    public void keepsMaxMessagesAndScrollsToBottom() throws Exception {
+        Skin skin = new Skin(Gdx.files.internal("skin/default.json"));
+        ChatBox box = new ChatBox(skin, mock(GameClient.class));
+        Stage stage = new Stage(new ScreenViewport(), mock(Batch.class));
+        stage.addActor(box);
+        final int size = 200;
+        stage.getViewport().update(size, size, true);
+
+        Field maxField = ChatBox.class.getDeclaredField("MAX_MESSAGES");
+        maxField.setAccessible(true);
+        int max = maxField.getInt(null);
+
+        final int extra = 5;
+        for (int i = 0; i < max + extra; i++) {
+            box.addMessage(new ChatMessage(i, "msg" + i));
+        }
+
+        Field logField = ChatBox.class.getDeclaredField("log");
+        logField.setAccessible(true);
+        com.badlogic.gdx.scenes.scene2d.ui.TextArea log =
+                (com.badlogic.gdx.scenes.scene2d.ui.TextArea) logField.get(box);
+        String[] lines = log.getText().split("\\n");
+        assertEquals(max, lines.length);
+        assertFalse(lines[0].startsWith("0:"));
+
+        stage.act(0f);
+
+        Field scrollField = ChatBox.class.getDeclaredField("scroll");
+        scrollField.setAccessible(true);
+        com.badlogic.gdx.scenes.scene2d.ui.ScrollPane scroll =
+                (com.badlogic.gdx.scenes.scene2d.ui.ScrollPane) scrollField.get(box);
+        assertEquals(scroll.getMaxY(), scroll.getScrollY(), 0f);
     }
 }
