1ec36672d8ae8d46c83bdc2735313b377c321939
Brett Dorrans <hello@lapidist.net>
2025-06-14 10:34:19 +0100

    feat(ui): update new game screen map size selection (#627)

--- client/src/main/java/net/lapidist/colony/client/screens/NewGameScreen.java
+++ client/src/main/java/net/lapidist/colony/client/screens/NewGameScreen.java
@@ -2,6 +2,7 @@ package net.lapidist.colony.client.screens;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
@@ -16,7 +17,6 @@ public final class NewGameScreen extends BaseScreen {
     private static final int SMALL_SIZE = 30;
     private static final int MEDIUM_SIZE = 60;
     private static final int LARGE_SIZE = 90;
-    private static final int HUGE_SIZE = 120;
     private final Colony colony;
     private int width = GameConstants.MAP_WIDTH;
     private int height = GameConstants.MAP_HEIGHT;
@@ -24,21 +24,25 @@ public final class NewGameScreen extends BaseScreen {
     public NewGameScreen(final Colony game) {
         this.colony = game;
 
-        Label label = new Label(I18n.get("newGame.saveName"), getSkin());
+        Label nameLabel = new Label(I18n.get("newGame.saveName"), getSkin());
         TextField nameField = new TextField("", getSkin());
         TextButton smallButton = new TextButton(I18n.get("newGame.sizeSmall"), getSkin());
         TextButton mediumButton = new TextButton(I18n.get("newGame.sizeMedium"), getSkin());
         TextButton largeButton = new TextButton(I18n.get("newGame.sizeLarge"), getSkin());
-        TextButton hugeButton = new TextButton(I18n.get("newGame.sizeHuge"), getSkin());
         TextButton startButton = new TextButton(I18n.get("newGame.start"), getSkin());
         TextButton backButton = new TextButton(I18n.get("common.back"), getSkin());
 
-        getRoot().add(label).row();
+        ButtonGroup<TextButton> sizeGroup = new ButtonGroup<>(smallButton, mediumButton, largeButton);
+        sizeGroup.setMaxCheckCount(1);
+        sizeGroup.setMinCheckCount(1);
+        sizeGroup.setUncheckLast(true);
+        smallButton.setChecked(true);
+
+        getRoot().add(nameLabel);
         getRoot().add(nameField).width(FIELD_WIDTH).row();
-        getRoot().add(smallButton).row();
-        getRoot().add(mediumButton).row();
+        getRoot().add(smallButton);
+        getRoot().add(mediumButton);
         getRoot().add(largeButton).row();
-        getRoot().add(hugeButton).row();
         getRoot().add(startButton).row();
         getRoot().add(backButton).row();
 
@@ -56,34 +60,33 @@ public final class NewGameScreen extends BaseScreen {
         smallButton.addListener(new ChangeListener() {
             @Override
             public void changed(final ChangeEvent event, final Actor actor) {
-                width = SMALL_SIZE;
-                height = SMALL_SIZE;
+                if (smallButton.isChecked()) {
+                    width = SMALL_SIZE;
+                    height = SMALL_SIZE;
+                }
             }
         });
 
         mediumButton.addListener(new ChangeListener() {
             @Override
             public void changed(final ChangeEvent event, final Actor actor) {
-                width = MEDIUM_SIZE;
-                height = MEDIUM_SIZE;
+                if (mediumButton.isChecked()) {
+                    width = MEDIUM_SIZE;
+                    height = MEDIUM_SIZE;
+                }
             }
         });
 
         largeButton.addListener(new ChangeListener() {
             @Override
             public void changed(final ChangeEvent event, final Actor actor) {
-                width = LARGE_SIZE;
-                height = LARGE_SIZE;
+                if (largeButton.isChecked()) {
+                    width = LARGE_SIZE;
+                    height = LARGE_SIZE;
+                }
             }
         });
 
-        hugeButton.addListener(new ChangeListener() {
-            @Override
-            public void changed(final ChangeEvent event, final Actor actor) {
-                width = HUGE_SIZE;
-                height = HUGE_SIZE;
-            }
-        });
 
         backButton.addListener(new ChangeListener() {
             @Override
