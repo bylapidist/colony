60b4af5d9854585661d7a165f155eebf7b670ed1
Brett Dorrans <hello@lapidist.net>
2025-06-14 11:48:40 +0100

    fix(config): remove map size from default config (#633)

--- core/src/main/java/net/lapidist/colony/components/GameConstants.java
+++ core/src/main/java/net/lapidist/colony/components/GameConstants.java
@@ -5,8 +5,9 @@ import net.lapidist.colony.config.ColonyConfig;
 public final class GameConstants {
     private GameConstants() { }
 
-    public static final int MAP_WIDTH = ColonyConfig.get().getInt("game.mapWidth");
-    public static final int MAP_HEIGHT = ColonyConfig.get().getInt("game.mapHeight");
+    // Default map size used before metadata is loaded
+    public static final int MAP_WIDTH = 30;
+    public static final int MAP_HEIGHT = 30;
     public static final int TILE_SIZE = ColonyConfig.get().getInt("game.tileSize");
     public static final int CHUNK_LOAD_RADIUS = ColonyConfig.get().getInt("game.chunkLoadRadius");
     public static final int NETWORK_BUFFER_SIZE = ColonyConfig.get().getInt("game.networkBufferSize");
