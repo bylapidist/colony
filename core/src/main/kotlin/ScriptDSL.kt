30f1ab39b11c8f11e1909640e2bd115e8b0e50d8
Brett Dorrans <hello@lapidist.net>
2025-06-15 08:58:18 +0100

    test: verify script DSL receives events (#717)

--- core/src/main/kotlin/ScriptDSL.kt
+++ core/src/main/kotlin/ScriptDSL.kt
@@ -1,14 +1,6 @@
 import net.lapidist.colony.events.Events
 import net.mostlyoriginal.api.event.common.Event
-import net.mostlyoriginal.api.event.common.Subscribe
 
 inline fun <reified T : Event> on(noinline handler: (T) -> Unit) {
-    Events.getInstance().registerEvents(
-        object {
-            @Subscribe
-            fun handle(event: T) {
-                handler(event)
-            }
-        },
-    )
+    Events.listen(T::class.java) { event -> handler(event) }
 }
