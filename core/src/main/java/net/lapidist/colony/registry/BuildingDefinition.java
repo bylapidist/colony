dd33803f6fcf569926bd0584debaef09785a219e
Brett Dorrans <hello@lapidist.net>
2025-06-18 22:06:32 +0100

    feat: add building descriptions and tooltips

--- core/src/main/java/net/lapidist/colony/registry/BuildingDefinition.java
+++ core/src/main/java/net/lapidist/colony/registry/BuildingDefinition.java
@@ -9,14 +9,35 @@ import net.lapidist.colony.components.state.ResourceData;
  * @param label  display label
  * @param asset  rendering asset reference
  * @param cost   resources required to construct the building
+ * @param description short localized description
  */
-public record BuildingDefinition(String id, String label, String asset, ResourceData cost) {
+public record BuildingDefinition(
+        String id,
+        String label,
+        String asset,
+        ResourceData cost,
+        String description
+) {
     public BuildingDefinition() {
-        this(null, null, null, new ResourceData());
+        this(null, null, null, new ResourceData(), null);
     }
 
     /** Convenience constructor omitting cost (defaults to zero). */
-    public BuildingDefinition(final String idValue, final String labelValue, final String assetValue) {
-        this(idValue, labelValue, assetValue, new ResourceData());
+    public BuildingDefinition(
+            final String idValue,
+            final String labelValue,
+            final String assetValue
+    ) {
+        this(idValue, labelValue, assetValue, new ResourceData(), null);
+    }
+
+    /** Constructor with custom cost, no description. */
+    public BuildingDefinition(
+            final String idValue,
+            final String labelValue,
+            final String assetValue,
+            final ResourceData costValue
+    ) {
+        this(idValue, labelValue, assetValue, costValue, null);
     }
 }
--- tests/src/test/java/net/lapidist/colony/client/ui/BuildMenuActorTest.java
+++ tests/src/test/java/net/lapidist/colony/client/ui/BuildMenuActorTest.java
@@ -67,4 +67,40 @@ public class BuildMenuActorTest {
         assertNotNull(button.findActor("stoneIcon"));
         assertNull(button.findActor("foodIcon"));
     }
+
+    @Test
+    public void tooltipAppearsOnHover() {
+        new BaseDefinitionsMod().init();
+        BuildPlacementSystem system = new BuildPlacementSystem(null, new KeyBindings());
+        com.artemis.World world = new com.artemis.World(new com.artemis.WorldConfigurationBuilder()
+                .with(new net.lapidist.colony.client.systems.PlayerCameraSystem(),
+                        new net.lapidist.colony.client.systems.CameraInputSystem(new KeyBindings()),
+                        system)
+                .build());
+        world.process();
+
+        Skin skin = new Skin(com.badlogic.gdx.Gdx.files.internal("skin/default.json"));
+        BuildMenuActor menu = new BuildMenuActor(skin, system, new GraphicsSettings());
+        Stage stage = new Stage(
+                new ScreenViewport(),
+                org.mockito.Mockito.mock(com.badlogic.gdx.graphics.g2d.Batch.class)
+        );
+        stage.addActor(menu);
+        menu.setVisible(true);
+        com.badlogic.gdx.scenes.scene2d.ui.Button button = menu.findActor("farm");
+        assertNotNull(button);
+        com.badlogic.gdx.scenes.scene2d.ui.Tooltip<?> tooltip = null;
+        for (com.badlogic.gdx.scenes.scene2d.EventListener l : button.getListeners()) {
+            if (l instanceof com.badlogic.gdx.scenes.scene2d.ui.Tooltip<?> t) {
+                tooltip = t;
+                break;
+            }
+        }
+        assertNotNull(tooltip);
+        com.badlogic.gdx.scenes.scene2d.InputEvent event = new com.badlogic.gdx.scenes.scene2d.InputEvent();
+        event.setListenerActor(button);
+        ((com.badlogic.gdx.scenes.scene2d.ui.Tooltip<?>) tooltip).enter(event, 0f, 0f, -1, null);
+        stage.act(0f);
+        assertTrue(((com.badlogic.gdx.scenes.scene2d.ui.Tooltip<?>) tooltip).getContainer().hasParent());
+    }
 }
