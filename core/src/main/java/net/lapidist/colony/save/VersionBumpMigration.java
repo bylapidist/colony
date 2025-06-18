f4c1f488263f81893054de6f3707aa91ee115f34
Brett Dorrans <hello@lapidist.net>
2025-06-18 08:04:08 +0100

    refactor: consolidate identity migrations

---
+++ core/src/main/java/net/lapidist/colony/save/VersionBumpMigration.java
@@ -0,0 +1,33 @@
+package net.lapidist.colony.save;
+
+import net.lapidist.colony.components.state.MapState;
+
+/**
+ * Simple migration that only updates the save version without altering the state.
+ */
+public final class VersionBumpMigration implements MapStateMigration {
+    private final int fromVersion;
+    private final int toVersion;
+
+    public VersionBumpMigration(final SaveVersion from, final SaveVersion to) {
+        this.fromVersion = from.number();
+        this.toVersion = to.number();
+    }
+
+    @Override
+    public int fromVersion() {
+        return fromVersion;
+    }
+
+    @Override
+    public int toVersion() {
+        return toVersion;
+    }
+
+    @Override
+    public MapState apply(final MapState state) {
+        return state.toBuilder()
+                .version(toVersion)
+                .build();
+    }
+}
